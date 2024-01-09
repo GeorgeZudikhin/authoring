faf69a52984f270ab17700435d2c9491d1e53814
Yehor Zudikhin <yehorzudikhin17@gmail.com>
2024-01-09 04:18:17 +0100

    Spring configuration

---
+++ src/main/java/com/project/authoring/AuthoringApplication.java
@@ -0,0 +1,32 @@
+package com.project.authoring;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@Log
+public class AuthoringApplication implements CommandLineRunner {
+
+	private final DataSource dataSource;
+
+	public AuthoringApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthoringApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) {
+		log.info("DataSource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
+
+}
